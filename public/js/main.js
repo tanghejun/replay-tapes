391a3ae2bca9c3c465a352b9b06364fe1bf20dc2
Tang,Hejun <tanghejun@baixing.com>
2016-11-23 16:54:23 +0800

    add tape duration/events summary to UI

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@
     angular.module('tapeStore', ['ngMaterial'])
         .controller('Ctrl', Ctrl)
         .factory('session', session)
+        .filter('duration', durationFilter)
 
     Ctrl.$inject = ['session']
 
@@ -16,6 +17,7 @@
         ctrl.search = searchTapes;
 
 
+
         function searchTapes() {
             var queryObj = {}
             queryObj.tags = ctrl.tags.join(',')
@@ -28,14 +30,60 @@
                 if(ctrl.tapes.length === 0) {
                     ctrl.message = 'Sorry, records not found.'
                 }
+                ctrl.tapes = ctrl.tapes.map(function(tape) {
+                    tape.duration = getDuration(tape)
+                    tape.summary = getEventsSummary(tape)
+                    return tape
+                })
             }).catch(function(err) {
                 ctrl.message = err.message
             }).finally(function() {
                 ctrl.loading = false
             })
         }
+        function getEventsSummary(tape) {
+            return tape.events.reduce(function(a, b) {
+                if( !a[b[0]] ) {
+                    a[b[0]] = 1
+                } else {
+                    a[b[0]]++;
+                }
+                return a;
+            }, {})
+        }
+
+        function getDuration(tape) {
+            var lastIndex = tape.events.length - 1;
+            return Math.ceil( ( getTime(tape.events[lastIndex]) - getTime(tape.events[0]) ) / 1000 )
+
+            function getTime(event) {
+                var arr = event.split(',')
+                var num = Number( arr[arr.length -1] )
+                if(num < 0) {
+                    num = 0
+                }
+                return num
+            }
+        }
     }
 
+    function durationFilter() {
+        return function(value) {
+            if(isNaN(value)) {
+                return value
+            }
+            var m = Math.floor( value / 60)
+            var s = value % 60
+            var str = ''
+            if(m) {
+                str += m + 'm'
+            }
+            if(s) {
+                str += s + 's'
+            }
+            return str;
+        }
+    }
 
     session.$inject = ['$http', '$httpParamSerializer']
 
@@ -50,7 +98,6 @@
         }
         function querySession(query) {
             var result = $httpParamSerializer(query)
-            console.log(result);
             return $http.get('/sessions?' + result)
         }
 
